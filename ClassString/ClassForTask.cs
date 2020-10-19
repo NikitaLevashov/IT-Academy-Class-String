c06c0de36f76e4c7ef4150e3653a8764db007473
NikitaLevashov <73133930+NikitaLevashov@users.noreply.github.com>
2020-10-19 21:40:01 +0300

    Add files via upload

---
+++ ClassString/ClassForTask.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassString
+{
+    static class ClassForTask
+    {
+        public static void ExclamationORInterrogative(string text) // Conclusion of exclamatory or interrogative sentences
+        {
+            string[] question = text.Split('?');
+            foreach (string st1 in question)
+            {
+                string[] split = st1.Split('!', '.');
+                Console.WriteLine(split[split.Length - 1] + "?"); //display interrogative sentences
+            }
+            string[] exclamation = text.Split('!');
+            foreach (string st2 in exclamation)
+            {
+                string[] split = st2.Split('?', '.');
+                Console.WriteLine(split[split.Length - 1] + '!');//display interrogative sentences
+            }
+        }
+        public static void MethodOFfindingNumbers(string text)
+        {
+            string sr = "";
+            string[] words = text.Split(new char[] { '!', '?', '.', ' ', }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (string st in words)
+            {
+                char[] mystr = st.ToCharArray();
+                for (int i = 0; i < mystr.Length; i++)
+                {
+                    if (char.IsNumber(mystr[i]))
+                    {
+                        sr += st + " ";
+                        break;
+                    }
+                }
+            }
+            Console.WriteLine("Result: " + sr);
+            
+        }
+        public static void SearchForCommas(string text)
+        {
+            string sr = " ";
+
+            string[] words = text.Split(new char[] { '!', '?', '.' }, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].Contains(','))
+                {
+                    sr += words[i] + "/";
+                }
+            }
+            Console.WriteLine(sr);
+        }
+        public static void ReplaceNumbersWithWords(string text)
+        {
+            text = text.Replace("1", "один").Replace("2", "два").Replace("3", "три").Replace("4", "пилюн").Replace("5", "пять").Replace("6", "шесть").Replace("7", "семь").Replace("8", "восемь").Replace("9", "девять").Replace("0", "нуль");
+            Console.WriteLine(text);
+        }
+        public static void PrintRepeatingWords(string text)
+        {
+
+            string[] strArray = text.Split(new[] { ' ', ',' },StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < strArray.Length; i++)
+            {
+                for (int j = 0; j < strArray.Length; j++)
+                {
+                    if (i != j && char.ToLower(strArray[i][0]) == char.ToLower(strArray[j][0]))
+                    {
+                        if (char.ToLower(strArray[i][strArray[i].Length - 1]) == char.ToLower(strArray[j][strArray[j].Length - 1]))
+                            Console.WriteLine(strArray[i]);
+                        break;
+                    }
+
+                }
+            }
+            Console.ReadKey();
+        }
+        
+    }
+}
---
+++ ClassString/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+
+namespace ClassString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello dear friends in the application number 2! Let's work on the topic System.String \n");
+            Console.WriteLine("Please enter text ");
+            bool flag = true;
+            string text = Console.ReadLine();
+            while (flag)
+            {
+                //input text
+                Console.WriteLine("Выберите пожалуйста следующее действие:\n 1 - Use split method;\n 2 - Display exclamations and questioning sentences \n 3 - Finding words in numbers;\n 4 - Finding sentences with commas;\n 5 - Replacing numbers in words with words;\n 6 - Finding words that begin and end with identical characters.\n 7 - Exit");
+                bool IsMenu = Int32.TryParse(Console.ReadLine(), out int e);
+
+                if (IsMenu == true)
+                {
+                    //Console.WriteLine("Выберите пожалуйста следующее действие:\n 1 - Use split method;\n 2 - Display exclamations and questioning sentences \n 3 - Finding words in numbers;\n 4 - Finding sentences with commas;\n 5 - Replacing numbers in words with words;\n 6 - Finding words that begin and end with identical characters.\n 7 - Exit");
+                    switch (e)
+                    {
+                        case 1:
+                            Console.WriteLine("Use split method: .  .  .");
+                            string[] words = text.Split(new char[] { ' ', ',', '!', '?', '.', ';' }, StringSplitOptions.RemoveEmptyEntries);// Break the string into an array of substrings
+                            foreach (string s in words)
+                                Console.WriteLine(s + " -  your substring contains " + s.Length + " characters");
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine("Please press any key.....");
+                            Console.ResetColor();
+
+                            Console.ReadKey(true);
+                            break;
+                        case 2:
+                            Console.WriteLine("Display exclamatory and interrogative sentences:.  .   .");
+                            ClassForTask.ExclamationORInterrogative(text);//display exclamations and questioning sentences
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine("Please press any key.....");
+                            Console.ResetColor();
+                            Console.ReadKey(true);
+                            break;
+
+                        case 3:
+                            Console.WriteLine("Finding words in numbers:.  .  .");
+                            ClassForTask.MethodOFfindingNumbers(text);// DISPLAY WORDS WITH NUMBERS
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine("Please press any key.....");
+                            Console.ResetColor();
+                            Console.ReadKey(true);
+                            break;
+                        case 4:
+                            Console.WriteLine("Finding sentences with commas:.  .   .");
+                            ClassForTask.SearchForCommas(text);
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine("Please press any key.....");
+                            Console.ResetColor();
+                            Console.ReadKey(true);
+                            break;
+                        case 5:
+                            Console.WriteLine("Replacing numbers in words with words:.   .    .");
+                            ClassForTask.ReplaceNumbersWithWords(text);
+                            Console.ReadKey(true);
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine("Please press any key.....");
+                            Console.ResetColor();
+                            break;
+                        case 6:
+                            Console.WriteLine("Finding words that begin and end with identical characters:.   .    .");
+                            ClassForTask.PrintRepeatingWords(text);
+                            Console.ReadKey(true);
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine("Please press any key.....");
+                            Console.ResetColor();
+                            break;
+                        case 7:
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine("*************Exit***************");
+                            Console.ResetColor();
+                            flag = false;
+                            break;
+                        default:
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine("You clicked an unknown letter. Press any key to repeat again");
+                            Console.ResetColor();
+                            break;
+       
+                    }
+                    
+                }
+                
+            }
+        }
+    }
+}
